0f4707ffc727adb0d38615f268624b98f131c181
hong yuseok <jericoh@naver.com>
2020-11-16 22:19:55 +0900

    lecture 3

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log(123);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "hys", age = 23, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log(123)
+const name ="hys", 
+    age=23, 
+    gender="male"
+
+const sayHi = (name: string, age: number, gender?:string) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age, gender)
+
+export {};
